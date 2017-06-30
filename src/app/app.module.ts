27f29c61f6aae6f733f6a15b1c17e23fd48b8181
Ben-MBA <moez.benaissa@gmail.com>
2017-06-30 15:58:59 +0200

    add new components header/footer/nav/home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HeaderComponent } from './main/header/header.component';
+import { NavigationComponent } from './main/navigation/navigation.component';
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
