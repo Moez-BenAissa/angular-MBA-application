5272f95d51a048ea9d56ce80235d8d660384da56
Moez Ben Aissa <moez.benaissa@gmail.com>
2017-07-10 17:04:13 +0200

    migrate css to scss

--- src/app/main/navigation/login-popin/login-popin.component.ts
+++ src/app/main/navigation/login-popin/login-popin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-popin',
   templateUrl: './login-popin.component.html',
-  styleUrls: ['./login-popin.component.css']
+  styleUrls: ['./login-popin.component.scss']
 })
 export class LoginPopinComponent implements OnInit {
 
