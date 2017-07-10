e8660a443b29325d397a83763b0d44dd45c74ce4
Moez Ben Aissa <moez.benaissa@gmail.com>
2017-07-10 17:03:32 +0200

    migrate css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular MBA Application';
+  title = 'Angular MBA Application'; 
 }
